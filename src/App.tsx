713962b59bc53dcdc3a601a9d579de35f3aca998
Sourabh Duadkiya <sdudakiya@gmail.com>
2025-01-13 22:25:42 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx or App.jsx
+import React from 'react';
+import TestAuth from './contexts/TestAuth'; 
+import { AuthProvider } from "./contexts/AuthContext";
+function App() {
+    return (
+        <AuthProvider>
+             <TestAuth />
+        </AuthProvider>
+    )
+}
+
+export default App;
