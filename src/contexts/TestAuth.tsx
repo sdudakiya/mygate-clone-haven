48029c07dd6a5facc2422089b3346c9f106f1e1d
Sourabh Duadkiya <sdudakiya@gmail.com>
2025-01-13 22:31:46 +0530

    Update TestAuth.tsx

--- src/contexts/TestAuth.tsx
+++ src/contexts/TestAuth.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useAuth } from "./contexts/AuthContext";
+import { useAuth } from "./AuthContext";
 
 function TestAuth() {
     const { user, isLoading } = useAuth();
