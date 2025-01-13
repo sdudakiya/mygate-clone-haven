99720454359b07809979f157c396df49d21223e9
Sourabh Duadkiya <sdudakiya@gmail.com>
2025-01-13 22:27:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import TestAuth from './contexts/TestAuth'; 
 import { AuthProvider } from "./contexts/AuthContext";
+import { useAuth } from "./contexts/AuthContext";
 function App() {
     return (
         <AuthProvider>
