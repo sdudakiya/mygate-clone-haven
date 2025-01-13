dcb620e096bd49f651ef84f7d13b06d2b41e965d
Sourabh Duadkiya <sdudakiya@gmail.com>
2025-01-13 22:23:17 +0530

    Create TestAuth.tsx

---
+++ src/contexts/TestAuth.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useAuth } from "./contexts/AuthContext";
+
+function TestAuth() {
+    const { user, isLoading } = useAuth();
+
+    if (isLoading) {
+    return <div>Loading...</div>;
+    }
+
+    if (user) {
+    return <div>User is logged in: {user.email}</div>;
+    } else {
+    return <div>User is not logged in.</div>;
+    }
+}
+
+export default TestAuth;
